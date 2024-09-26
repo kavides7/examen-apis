585b6f5fc570960e912a73d4be532dc2e766be35
kavides7 <kvidesg@miumg.edu.gt>
2024-09-25 18:45:41 -0600

    x

--- app/models/usuario.model.js
+++ app/models/usuario.model.js
@@ -1,30 +1,24 @@
-const Usuario = (sequelize, Sequelize) => {
-    const Usuario = sequelize.define('Usuario', {
+module.exports = (sequelize, Sequelize) => {
+    const Usuario = sequelize.define("usuario", {
         id_usuario: {
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true
         },
         nombre: {
-            type: Sequelize.STRING,
-            allowNull: false // Asegúrate de que este campo no sea nulo
+            type: Sequelize.STRING
         },
         email: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true // Para evitar duplicados
+            type: Sequelize.STRING
         },
+        
         contraseña: {
-            type: Sequelize.STRING, // Cambiado a STRING para almacenar contraseñas
-            allowNull: false // Asegúrate de que este campo no sea nulo
+            type: Sequelize.INTEGER
         },
         fecha_ingreso: {
-            type: Sequelize.DATE, // Cambiado a DATE para almacenar correctamente la fecha
-            defaultValue: Sequelize.NOW // Valor por defecto para la fecha de ingreso
+            type: Sequelize.INTEGER
         }
-    });
 
+    });
     return Usuario;
 };
-
-module.exports = Usuario;
